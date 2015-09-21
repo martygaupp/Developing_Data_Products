70dbb22e549fc2c554f9cd6bf93a21df747336ab
martygaupp <martygaupp@gmail.com>
2015-09-20 19:07:50 -0600

    Create server.R

---
+++ server.R
@@ -0,0 +1,31 @@
+library(manipulate)
+library(ggplot2)
+mu0 = 30
+
+shinyServer(
+  function(input, output) {
+    output$newGraph <- renderPlot({
+      sigma <- input$sigma
+      mua <- input$mua
+      n <- input$n
+      alpha <- input$alpha
+      g = ggplot(data.frame(mu = c(27, 36)), aes(x = mu))
+      g = g + stat_function(fun=dnorm, geom = "line", 
+                            args = list(mean = mu0, sd = sigma / sqrt(n)), 
+                            size = 2, col = "red")
+      g = g + stat_function(fun=dnorm, geom = "line", 
+                            args = list(mean = mua, sd = sigma / sqrt(n)), 
+                            size = 2, col = "blue")
+      xitc = mu0 + qnorm(1 - alpha) * sigma / sqrt(n)
+      conf = 100*(1 - alpha)
+      g = g + annotate("text", x = 27, y = .35, label = paste("alpha =", format(round(alpha,3), nsmall = 3)), hjust = 0)
+      g = g + annotate("text", x = 27.25, y = .325, label = paste("confidence =", format(round(conf,1), nsmall = 1), "%"), hjust = 0)
+      beta = pnorm(xitc, mean = mua, sd = sigma/sqrt(n), lower.tail = TRUE)
+      power = 100*(1 - beta)
+      g = g + annotate("text", x = 27, y = .3, label = paste("beta =", format(round(beta,3), nsmall = 3)), hjust = 0)
+      g = g + annotate("text", x = 27.25, y = .275, label = paste("power =", format(round(power,1), nsmall = 1), "%"), hjust = 0)
+      g = g + geom_vline(xintercept=xitc, size = 3)
+      g
+    })
+  }
+)
